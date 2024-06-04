b45223e2282f718c6ba644bc4f081a03edf4a028
Victor Medrado <victormedrado49@gmail.com>
2024-06-04 10:18:29 -0300

    Estrutura visual - Criando primeiro componente Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Banner from './components/Banner';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner/index.js
@@ -0,0 +1,9 @@
+import './styles/index.css'
+
+export default function Banner() {
+  return (
+    <header className='banner'>
+      <img src="/images/banner.png" alt="Banner principal" />
+    </header>
+  )
+}
